6af5df26dd8fcd5cd2da7efd8c6d1dbc818d6021
ramofx <roman.dvorovoy123@gmail.com>
2021-06-01 21:59:59 +0200

    update: moved

---
+++ Modules/UI/Dialogs.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Forms;
+
+using SnakeGame.Modules.Elements.Complex;
+
+
+
+namespace SnakeGame.Modules.UI {
+  static class Dialogs {
+    public static void GameWon(Player winner) {
+      MessageBox.Show($"{ winner.Name } is the winner! Congratulations!", "And the winner is...");
+    }
+
+    public static void AskForNewGame() {
+      DialogResult dialogResult = MessageBox.Show(
+        "Would you like to start a new game?",
+        "Another one?",
+        MessageBoxButtons.YesNo
+      );
+
+      if (dialogResult == DialogResult.Yes) {
+        Application.Restart();
+      } else {
+        Application.Exit();
+      }
+    }
+  }
+}
