e8c61fffd368d1ad48281411e7a097e0d9f52fc4
ramofx <roman.dvorovoy123@gmail.com>
2021-06-01 22:20:13 +0200

    refactor: simplified

---
+++ Modules/Core/Loop.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+
+using SnakeGame.Modules.Helpers;
+
+
+
+namespace SnakeGame.Modules.Core {
+  class Loop {
+    public readonly Timer Timer;
+
+    public Loop() {
+      this.Timer = new Timer {
+        Interval = 1000 / Storage.Preferences.FramesPerSecond
+      };
+      this.Timer.Tick += this.GameTick;
+    }
+
+    public void GameTick(object sender, EventArgs e) {
+      // Fire frame render
+      Storage.Controls.Find("Panel_Arena", true)[0].Invalidate();
+    }
+  }
+}
