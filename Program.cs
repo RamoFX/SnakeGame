0d88f4df09e0b84164661357275eca5806e75ea5
ramofx <roman.dvorovoy123@gmail.com>
2021-06-01 22:01:14 +0200

    update: renamed

--- Program.cs
+++ Program.cs
@@ -1,19 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System;
 using System.Windows.Forms;
 
-namespace Snake {
+
+
+namespace SnakeGame {
   static class Program {
     /// <summary>
-    /// The main entry point for the application.
+    ///  The main entry point for the application.
     /// </summary>
     [STAThread]
     static void Main() {
+      //Application.SetHighDpiMode(HighDpiMode.SystemAware);
       Application.EnableVisualStyles();
       Application.SetCompatibleTextRenderingDefault(false);
-      Application.Run(new Window());
+      Application.Run(new SnakeGame.Modules.UI.Windows.Main());
     }
   }
 }
