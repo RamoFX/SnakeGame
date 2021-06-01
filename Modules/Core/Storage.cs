040e25d3c4a61fbef195e6dddbb5aa8ab5c23673
ramofx <roman.dvorovoy123@gmail.com>
2021-06-01 22:27:44 +0200

    implemented: storage

---
+++ Modules/Core/Storage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+
+
+namespace SnakeGame.Modules.Core {
+  class Storage {
+    public static Graphics Graphics;
+    public static Control.ControlCollection Controls;
+
+    public static Preferences Preferences;
+    public static Runtime Runtime;
+    public static Loop Loop;
+
+    public static Random Random;
+  }
+}
