9bc5032e66ac94303a53480fab2d0c0d04618975
ramofx <roman.dvorovoy123@gmail.com>
2021-06-01 22:25:32 +0200

    refactor: structurized

---
+++ Modules/Core/Preferences.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+
+
+
+namespace SnakeGame.Modules.Core {
+  class Preferences {
+    public readonly int ScaleFactor;
+    public readonly Size ArenaSize;
+    public readonly (int, int) GrowPerFood;
+    public readonly int FramesPerSecond;
+
+    public Preferences(
+      int? scaleFactor = null,
+      Size? arenaSize = null,
+      (int, int)? growPerFood = null,
+      int? framesPerSecond = null
+    ) {
+      this.ScaleFactor = scaleFactor ?? 32;
+      this.ArenaSize = arenaSize ?? new(16, 16);
+      this.GrowPerFood = growPerFood ?? (1, 5);
+      this.FramesPerSecond = framesPerSecond ?? 30;
+    }
+  }
+}
---
+++ Modules/Core/Runtime.cs
@@ -0,0 +1,47 @@
+﻿using System.Drawing;
+
+using SnakeGame.Modules.Elements.Complex;
+using SnakeGame.Modules.Extensions;
+using SnakeGame.Modules.Helpers;
+
+
+
+namespace SnakeGame.Modules.Core {
+  class Runtime {
+    public GameStates GameState;
+    public readonly Player[] Players;
+
+    public Runtime() {
+      this.GameState = GameStates.Awaiting;
+
+      this.Players = new Player[2] {
+        new(
+          "Blue",
+          "player1",
+
+          // Food
+          Storage.Preferences.ArenaSize.GetCenter().GetOffset(new Point(-5, -2)),
+          Brushes.DarkBlue,
+
+          // Snake
+          Storage.Preferences.ArenaSize.GetCenter().GetOffset(new Point(-5, 2)),
+          Positioning.Direction.Up,
+          Brushes.Blue
+        ),
+        new(
+          "Red",
+          "player2",
+
+          // Food
+          Storage.Preferences.ArenaSize.GetCenter().GetOffset(new Point(5, 2)),
+          Brushes.DarkRed,
+
+          // Snake
+          Storage.Preferences.ArenaSize.GetCenter().GetOffset(new Point(5, -2)),
+          Positioning.Direction.Down,
+          Brushes.Red
+        )
+      };
+    }
+  }
+}
