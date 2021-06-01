ed2e366100b45d0ea06c1cf40025b1aa108a5acc
ramofx <roman.dvorovoy123@gmail.com>
2021-06-01 22:31:05 +0200

    implemented: positioning helper

--- Modules/Helpers/Positioning.cs
+++ Modules/Helpers/Positioning.cs
@@ -1,84 +1,72 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
-namespace Snake {
-  public partial class Window : Form {
-    class Positioning {
-      private static Size GetUnscaledGameFieldSize() {
-        return Scaling.Unscale(GameField.Size);
-      } 
+using SnakeGame.Modules.Core;
+using SnakeGame.Modules.Elements.Base;
 
-      // Center
-      public static Point Center() {
-        return new Point(
-          Scaling.Divide(GetUnscaledGameFieldSize(), 2)
-        );
+
+
+namespace SnakeGame.Modules.Helpers {
+  static class Positioning {
+    // Random
+    public static Point Random() {
+      return new Point(
+        Storage.Random.Next(Storage.Preferences.ArenaSize.Width),
+        Storage.Random.Next(Storage.Preferences.ArenaSize.Height)
+      );
+    }
+
+    public static Point Random(Point excludePoint) {
+      Point randomPoint = Random();
+
+      while (randomPoint == excludePoint) {
+        randomPoint = Random();
       }
 
-      public static Point Center(int offset_x, int offset_y) {
-        Size DividedUnscaledGameFieldSize = Scaling.Divide(GetUnscaledGameFieldSize(), 2);
+      return randomPoint;
+    }
 
-        return new Point(
-          DividedUnscaledGameFieldSize.Width + offset_x,
-          DividedUnscaledGameFieldSize.Height + offset_y
-        );
+    public static Point Random(IEnumerable<Point> excludePoints) {
+      Point randomPoint = Random();
+
+      while (excludePoints.Contains(randomPoint)) {
+        randomPoint = Random();
       }
 
-      // Random
-      public static Point Random() {
-        return new Point(
-          Window.Random.Next(GetUnscaledGameFieldSize().Width),
-          Window.Random.Next(GetUnscaledGameFieldSize().Height)
-        );
+      return randomPoint;
+    }
+
+    public static Point Random(IEnumerable<Element> excludeElements) {
+      List<Point> excludePoints = excludeElements
+        .Select( (Element element) => element.Location )
+        .ToList();
+
+      Point randomPoint = Random(excludePoints);
+
+      return randomPoint;
+    }
+
+    // Direction
+    public enum Direction { Up, Down, Left, Right }
+
+    public static Direction OppositeDirection(Direction direction) {
+      return (Direction) (int) direction + ( (int) direction % 2 == 0 ? 1 : -1 );
+    }
+
+    public static Direction RandomDirection() {
+      return (Direction) Storage.Random.Next(Enum.GetValues(typeof(Direction)).Length);
+    }
+
+    public static Direction RandomDirection(Direction excludeDirection) {
+      Direction result = RandomDirection();
+
+      while (result == excludeDirection) {
+        result = RandomDirection();
       }
 
-      public static Point Random(Point excludePoint) {
-        Point randomPoint = Random();
-
-        while (randomPoint == excludePoint) {
-          randomPoint = Random();
-        }
-
-        return randomPoint;
-      }
-
-      public static Point Random(IEnumerable<Point> excludePoints) {
-        Point randomPoint = Random();
-
-        while (excludePoints.Contains(randomPoint)) {
-          randomPoint = Random();
-        }
-
-        return randomPoint;
-      }
-
-      public static Point Random(IEnumerable<Element> excludeElements) {
-        List<Point> excludePoints = excludeElements
-                                      .Select((Element element) => element.Location)
-                                      .ToList();
-
-        Point randomPoint = Random(excludePoints);
-
-        return randomPoint;
-      }
-
-      // Direction
-      public enum Direction { Up, Down, Left, Right }
-
-      public static Direction OppositeDirection(Direction direction) {
-        return (Direction) (int) direction + ( (int) direction % 2 == 0 ? 1 : -1 );
-      }
-
-      public static Direction RandomDirection() {
-        return (Direction) Window.Random.Next(Enum.GetValues(typeof(Direction)).Length);
-      }
+      return result;
     }
   }
 }
